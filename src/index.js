f47172c3f2a5e8b21e3006b690d6575f2d6ec189
Asqar Arslonov <asqararslonov2008@gmail.com>
2021-07-22 17:35:14 +0500

    added array

--- src/index.js
+++ src/index.js
@@ -11,11 +11,16 @@ const SearchInput = () => {
 }
 
 const TodoList = () => {
+  const data = ["Learn React", "Build awesome App"]
+
+  const data2 = <li>Drink Coffe</li>
+
   return  (  
   <div> 
       <ul>
-        <li>React O'rganish {console.log("Hello")} </li> 
-        <li>Build aweosme App</li>
+        <li>{data[0]} </li> 
+        <li>{data[1]}</li>
+       {data2}
       </ul>
   </div>
   )
