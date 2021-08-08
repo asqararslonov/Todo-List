7508625bcf1d16c8c3296151c2788af9e645403a
Asqar Arslonov <asqararslonov2008@gmail.com>
2021-08-08 14:10:03 +0500

    index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import App from './components/App/App';
-
 import "./index.css";
-
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.css";
-
 
 ReactDOM.render(<App />, document.getElementById("root"));
